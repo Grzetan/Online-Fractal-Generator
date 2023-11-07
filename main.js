2d0c763af0555f17ca54b0b44fa6c1f5d35fdbf5
Grzegorz_Paleta <g.paleta@softint.eu>
2023-11-07 13:03:21 +0100

    Added geenrating fractal from input

--- main.js
+++ main.js
@@ -2,7 +2,138 @@ var canvas = document.getElementById("canvas");
 let button_submit = document.getElementById("formula-submit");
 let formula_input = document.getElementById("formula");
 
-function generateFractal(){
+const OPERATORS = {'-': {code: 'subtract(', associativity: 'left', precedence: 1}, 
+                   '+': {code: 'add(', associativity: 'left', precedence: 1}, 
+                   '/': {code: 'div(', associativity: 'left', precedence: 2}, 
+                   '*': {code: 'mul(', associativity: 'left', precedence: 2}, 
+                   '^': {code: 'power(', associativity: 'right', precedence: 3}};
+
+const FUNCTIONS = {
+    'len': {code: 'len()'},
+    'abs': {code: 'abs()'},
+    'sin': {code: 'sin()'},
+    'conj': {code: 'conjugate()'},
+    'neg': {code: 'neg()'},
+    'inv': {code: 'inverse()'}
+
+}
+
+class Operation{
+    constructor(type, first, second){
+        this.type = type;
+        this.first = first;
+        this.second = second;
+    }
+
+    toString(){
+        return OPERATORS[this.type].code + this.first + ', ' + this.second + ")";
+    }
+}
+
+function removeAfterChar(inputString, charToRemove) {
+  const charIndex = inputString.indexOf(charToRemove);
+  
+  if (charIndex !== -1) {
+    return inputString.substring(0, charIndex);
+  }
+  
+  return inputString;
+}
+
+function extractStringInParentheses(inputString) {
+  const regex = /\(([^)]+)\)/;
+  const match = inputString.match(regex);
+
+  if (match && match.length > 1) {
+    return match[1];
+  }
+
+  return null;
+}
+
+function RPN2Code(formula){
+  let splitted = formula.split(' ')
+  console.log(splitted);
+
+  for(let i=0; i<splitted.length; i++){
+      let operation = removeAfterChar(splitted[i], '(');
+      if(Object.keys(FUNCTIONS).includes(operation)){
+          let content = extractStringInParentheses(splitted[i]);
+          console.log(content);
+          splitted[i] = content + "." + FUNCTIONS[operation].code;
+      }
+  }
+
+  let stack = [];
+  let mainOperation = new Operation();
+
+  for(let i=0; i<splitted.length; i++){
+      if(Object.keys(OPERATORS).includes(splitted[i])){
+          let second = stack.pop();
+          let first = stack.pop();
+          let operation = new Operation(splitted[i], first, second);
+          stack.push(operation.toString());
+      }else{
+          stack.push(splitted[i]);
+      }
+  }
+
+  let output = stack[0].toString();
+
+  return output;
+}
+
+// https://www.andreinc.net/2010/10/05/converting-infix-to-rpn-shunting-yard-algorithm
+function formula2RPN(formula){
+  splitted = formula.split(' ');
+  output = [];
+  stack = [];
+  for(let i=0; i<splitted.length; i++){
+      if(Object.keys(OPERATORS).includes(splitted[i])){
+          while(stack.length != 0 && Object.keys(OPERATORS).includes(stack[stack.length-1])){
+              const curr = OPERATORS[splitted[i]];
+              const next = OPERATORS[stack[stack.length-1]];
+              if((curr.associativity == 'left' && next.precedence >= curr.precedence) ||
+                  (curr.associativity == 'right' && next.precedence > curr.precedence)){
+                  output.push(stack.pop());
+                  continue;
+              }
+              break;
+          }
+          stack.push(splitted[i]);
+      }else if(splitted[i] == '('){
+          stack.push(splitted[i]);
+      }else if(splitted[i] == ')'){
+          while(stack.length != 0 && stack[stack.length-1] != '('){
+              output.push(stack.pop());
+          }
+          stack.pop();
+      }else{
+          output.push(splitted[i]);
+      }
+  }
+
+  while(stack.length != 0){
+      output.push(stack.pop());
+  }
+
+  return output.join(' ');
+}
+
+function formula2Code(formula){
+  return RPN2Code(formula2RPN(formula));
+}
+
+function getFragmentShaderWithFormula(formula){
+  let code = window.newtonFragmentShader;
+
+  return code.replaceAll("vec2(0.0) //PASTE FORMULA HERE", formula + ";");
+}
+
+function generateFractal(formula){
+  const fractalCode = formula2Code(formula);
+  console.log(fractalCode);
+
   var gl = canvas.getContext("webgl");
 
   if (!gl) {
@@ -25,7 +156,7 @@ function generateFractal(){
   
   // Feed GPU with FRAGMENT shader and compile
   const fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragShader, window.newtonFragmentShader);
+  gl.shaderSource(fragShader, getFragmentShaderWithFormula(fractalCode));
   gl.compileShader(fragShader);
   console.log(gl.getShaderInfoLog(fragShader))
   
@@ -59,7 +190,7 @@ button_submit.addEventListener('click', (e)=>{
   generateFractal(formula_input.textContent);
 });
 
-generateFractal("( z ^ 3 - 1 ) / ( 3 * z ^ 2 )");
+generateFractal("( z ^ vec2(3.0,0.0) - vec2(1.0,0.0) ) / ( vec2(3.0,0.0) * z ^ vec2(2.0,0.0) )");
 
 // var ctx = canvas.getContext('2d');
 // var WIDTH = canvas.width;
@@ -74,125 +205,6 @@ generateFractal("( z ^ 3 - 1 ) / ( 3 * z ^ 2 )");
 // let ymax = 2;
 // let maxIterations = 20;
 
-// const OPERATORS = {'-': {code: 'sub(', associativity: 'left', precedence: 1}, 
-//                    '+': {code: 'add(', associativity: 'left', precedence: 1}, 
-//                    '/': {code: 'div(', associativity: 'left', precedence: 2}, 
-//                    '*': {code: 'mul(', associativity: 'left', precedence: 2}, 
-//                    '^': {code: 'pow(', associativity: 'right', precedence: 3}};
-
-// const FUNCTIONS = {
-//     'len': {code: 'len()'},
-//     'abs': {code: 'abs()'},
-//     'sin': {code: 'sin()'},
-//     'conj': {code: 'conjugate()'},
-//     'neg': {code: 'neg()'},
-//     'inv': {code: 'inverse()'}
-
-// }
-
-// class Operation{
-//     constructor(type, first, second){
-//         this.type = type;
-//         this.first = first;
-//         this.second = second;
-//     }
-
-//     toString(){
-//         return OPERATORS[this.type].code + this.first + ', ' + this.second + ")";
-//     }
-// }
-
-// function removeAfterChar(inputString, charToRemove) {
-//   const charIndex = inputString.indexOf(charToRemove);
-  
-//   if (charIndex !== -1) {
-//     return inputString.substring(0, charIndex);
-//   }
-  
-//   return inputString;
-// }
-
-// function extractStringInParentheses(inputString) {
-//   const regex = /\(([^)]+)\)/;
-//   const match = inputString.match(regex);
-
-//   if (match && match.length > 1) {
-//     return match[1];
-//   }
-
-//   return null;
-// }
-
-// function polishNotation2Code(formula){
-//     let splitted = formula.split(' ')
-//     console.log(splitted);
-
-//     for(let i=0; i<splitted.length; i++){
-//         let operation = removeAfterChar(splitted[i], '(');
-//         if(Object.keys(FUNCTIONS).includes(operation)){
-//             let content = extractStringInParentheses(splitted[i]);
-//             console.log(content);
-//             splitted[i] = content + "." + FUNCTIONS[operation].code;
-//         }
-//     }
-
-//     let stack = [];
-//     let mainOperation = new Operation();
-
-//     for(let i=0; i<splitted.length; i++){
-//         if(Object.keys(OPERATORS).includes(splitted[i])){
-//             let second = stack.pop();
-//             let first = stack.pop();
-//             let operation = new Operation(splitted[i], first, second);
-//             stack.push(operation.toString());
-//         }else{
-//             stack.push(splitted[i]);
-//         }
-//     }
-
-//     let output = stack[0].toString();
-
-//     console.log(output);
-
-//     return new Function("z", "c", "return " + output);
-// }
-
-// // https://www.andreinc.net/2010/10/05/converting-infix-to-rpn-shunting-yard-algorithm
-// function formula2RPN(formula){
-//     splitted = formula.split(' ');
-//     output = [];
-//     stack = [];
-//     for(let i=0; i<splitted.length; i++){
-//         if(Object.keys(OPERATORS).includes(splitted[i])){
-//             while(stack.length != 0 && Object.keys(OPERATORS).includes(stack[stack.length-1])){
-//                 const curr = OPERATORS[splitted[i]];
-//                 const next = OPERATORS[stack[stack.length-1]];
-//                 if((curr.associativity == 'left' && next.precedence >= curr.precedence) ||
-//                    (curr.associativity == 'right' && next.precedence > curr.precedence)){
-//                     output.push(stack.pop());
-//                     continue;
-//                 }
-//                 break;
-//             }
-//             stack.push(splitted[i]);
-//         }else if(splitted[i] == '('){
-//             stack.push(splitted[i]);
-//         }else if(splitted[i] == ')'){
-//             while(stack.length != 0 && stack[stack.length-1] != '('){
-//                 output.push(stack.pop());
-//             }
-//             stack.pop();
-//         }else{
-//             output.push(splitted[i]);
-//         }
-//     }
-
-//     while(stack.length != 0){
-//         output.push(stack.pop());
-//     }
-
-//     return output.join(' ');
-// }
 
 // let formula = "( z ^ 4.0 - vec2(1.0) ) / ( z ^ 3.0 * vec2(4.0) )";
 // const RPN = formula2RPN(formula);
--- netwonFractal.js
+++ netwonFractal.js
@@ -1,22 +1,25 @@
 window.newtonFragmentShader = `
 precision mediump float;
 
-#define add(a, b) vec2(a.x+b.x, a.y+b.x)
-#define substract(a, b) vec2(a.x-b.x, a.y-b.x)
-#define mul(a, b) vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x)
-#define div(a, b) vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)))
-#define abs(a) length(a)
-
-vec2 power(vec2 a, float n) {
-  float angle = atan(a.y, a.x);
-  float r = length(a);
-  float real = pow(r, n) * cos(n*angle);
-  float im = pow(r, n) * sin(n*angle);
-  return vec2(real, im);
+vec2 add(vec2 a, vec2 b){
+  return a + b;
 }
 
+vec2 subtract(vec2 a, vec2 b){
+  return a - b;
+}
+
+vec2 mul(vec2 a, vec2 b){
+  return vec2(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x);
+}
+
+vec2 div(vec2 a, vec2 b){
+  return vec2(((a.x*b.x+a.y*b.y)/(b.x*b.x+b.y*b.y)),((a.y*b.x-a.x*b.y)/(b.x*b.x+b.y*b.y)));
+}
+
+
 vec2 power(vec2 z, vec2 w) {
-  float r = abs(z);
+  float r = length(z);
   float theta = atan(z.y, z.x);
 
   float r_ = pow(r, w.x);
@@ -41,9 +44,11 @@ void main() {
   for (int i = 0; i < 100; i++) {
     if (escaped) break;
 
-    vec2 delta = div((power(z, 3.0) - vec2(1.0, 0.0)), (power(z, 2.0) * 3.0));
+    vec2 delta = vec2(0.0) //PASTE FORMULA HERE
+    
+    // div(subtract(power(z, vec2(3.0, 0.0)), vec2(1.0, 0.0)), mul(power(z, vec2(2.0, 0.0)), vec2(3.0, 0.0)));
 
-    if (abs(delta) < 1e-6) {
+    if (length(delta) < 1e-6) {
       escaped = true;
     
       gl_FragColor = vec4((z.x + 2.0) / 4.0, (z.y + 2.0) / 4.0, 1.0, 1.0);
